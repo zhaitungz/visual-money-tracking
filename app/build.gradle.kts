11bec385d9c36ecabe4858f52538ea8efd4a9930
unknown <zhaitungz@gmail.com>
2026-03-30 17:39:57 +0700

    fix: bump compileSdk to 35, AGP to 8.5.0, add stacktrace to workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.visualmoneytracker"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.visualmoneytracker"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
